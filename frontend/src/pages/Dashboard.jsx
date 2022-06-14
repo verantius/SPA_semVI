3152d3ad8fe3674c6b83f963a2cb70fbee4d4bdc
Adas <adam.misiag@live.com>
2022-06-14 12:36:18 +0200

    ok

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -48,7 +48,7 @@ function Dashboard() {
             ))}
           </div>
         ) : (
-          <h3>You have not set any goals</h3>
+          <h3>Nie dodałeś jeszcze badań</h3>
         )}
       </section>
     
